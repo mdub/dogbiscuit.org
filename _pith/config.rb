51fd53880cc0b512175e363ffe8d6abdbe9abca1
Mike Williams <mdub@dogbiscuit.org>
2010-09-02 09:49:09 +1000

    Add publication dates to the blog listing, and start to style things up a little.

--- _pith/config.rb
+++ _pith/config.rb
@@ -1,2 +1,25 @@
 # Handle legacy blog-entries in "WikiWah" format
 require 'wikiwah/tilt_integration'
+
+require "time"
+
+class Pith::Input::Template 
+  
+  def publication_date
+    published = meta["published"]
+    Time.parse(published) if published
+  end
+  
+end
+
+project.helpers do
+  
+  def blog_entries
+    project.inputs.select do |input|
+      input.respond_to?(:publication_date) && !input.publication_date.nil?
+    end.compact.sort_by do |input|
+      input.publication_date
+    end.reverse
+  end
+  
+end
