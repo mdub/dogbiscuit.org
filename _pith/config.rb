18f805fc69b32a17e2a0901af82ced3cbf8e9b1c
Mike Williams <mdub@dogbiscuit.org>
2010-09-02 21:44:40 +1000

    JUST the weblog entries.

--- _pith/config.rb
+++ _pith/config.rb
@@ -13,10 +13,10 @@ class Pith::Input::Template
 end
 
 project.helpers do
-  
+
   def blog_entries
     project.inputs.select do |input|
-      input.respond_to?(:publication_date) && !input.publication_date.nil?
+      input.path.to_s =~ /^weblog/ && input.respond_to?(:publication_date) && !input.publication_date.nil?
     end.compact.sort_by do |input|
       input.publication_date
     end.reverse
