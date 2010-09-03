3c25fa3717091ff0a39ea9032a1eb771ef801946
Mike Williams <mdub@dogbiscuit.org>
2010-09-03 22:38:19 +1000

    Move /weblog to /mdub/weblog.

--- _pith/config.rb
+++ _pith/config.rb
@@ -12,11 +12,26 @@ class Pith::Input::Template
   
 end
 
+class ::String
+  def starts_with?(prefix)
+    prefix = prefix.to_s
+    self[0, prefix.length] == prefix
+  end
+end
+
+class ::Pathname
+  def within?(ancestor_path)
+    self.to_str.starts_with?(ancestor_path.to_str + "/")
+  end
+end
+
 project.helpers do
 
-  def blog_entries
+  def published_pages
     project.inputs.select do |input|
-      input.path.to_s =~ /^weblog/ && input.respond_to?(:publication_date) && !input.publication_date.nil?
+      input.path.within?(current_input.path.parent)
+    end.select do |input|
+      input.respond_to?(:publication_date) && !input.publication_date.nil?
     end.compact.sort_by do |input|
       input.publication_date
     end.reverse
